58cfe584d26d8924098bfedda20777f05b1f719f
jobootwori <jobootwori@gmail.com>
2022-07-05 17:35:26 +0300

    UPDATE: Initial commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""number_of_lines
+"""
+
+def number_of_lines(filename=""):
+    """Takes in str filename to read the number of lines
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        no_lines = 0
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            n0_lines += 1
+        return no_lines
