16d4efb7c4f33c198edc596c2d75e6dfc739e414
jobootwori <jobootwori@gmail.com>
2022-09-06 13:44:32 +0300

    UPDATE: Correcting Changes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // JS Script
 
-let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
