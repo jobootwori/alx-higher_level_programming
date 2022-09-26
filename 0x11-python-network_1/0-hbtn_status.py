dde5d9a4fca63cb4d76722fca2e6bb0e3c3e284e
jobootwori <jobootwori@gmail.com>
2022-09-26 12:44:16 +0300

    UPDATE: Changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 
 def fetcher():
-    """fetcher"""
+    """fetcher of the url"""
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
